fd656b3fd53821f427d78086d52082645aab879e
OreoPL-studio <81591050+OreoPL-studio@users.noreply.github.com>
2021-05-22 21:22:27 +0200

    Strona pobierania jest
    mało dla was dużo dla mnie XD

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -1,10 +1,19 @@
 import * as styles from "../styles/Download.module.css"
 
-export default function Download()
-{
-    return(
-        <div className="download">
-            {/* Miejsce dla Pawła */}
+export default function Download() {
+    return (
+        <div>
+            <i className={styles.downloadONE + " fab fa-windows"}></i>
+            <i className={styles.downloadTWO + " fab fa-apple"}> </i>
+            <i className={styles.downloadTHREE + " fab fa-linux"}> </i>
+            <div className={styles.downloadTEXT}>
+
+                <hr1 className={styles.downloadTEXT_ONE}>Windows</hr1>
+                <hr2 className={styles.downloadTEXT_TWO}>MAC</hr2>
+                <hr3 className={styles.downloadTEXT_THREE}><b>Linux</b></hr3>
+
+            </div>
         </div>
+
     )
 }
