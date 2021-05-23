5336b0edc9204a3808a89170d0ee4b4040e21996
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2021-05-23 23:51:51 +0200

    Dodano linki do pobierania

--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -7,11 +7,15 @@ export default function Download() {
             <i className={styles.downloadTWO + " fab fa-apple"}> </i>
             <i className={styles.downloadTHREE + " fab fa-linux"}> </i>
             <div className={styles.downloadTEXT}>
-
-                <hr1 className={styles.downloadTEXT_ONE}>Windows</hr1>
-                <hr2 className={styles.downloadTEXT_TWO}>MAC</hr2>
-                <hr3 className={styles.downloadTEXT_THREE}><b>Linux</b></hr3>
-
+                <a href="https://github.com/B-P-Coders/covid-clicker-desktop/releases/download/v1.0.0/covid-clicker_win-64_1.0.0.exe">
+                    <h1 className={styles.downloadTEXT_ONE}>Windows</h1>
+                </a>
+                <a href="https://github.com/B-P-Coders/covid-clicker-desktop/releases/download/v1.0.0/covid-clicker_osx-64_1.0.0.dmg">
+                    <h2 className={styles.downloadTEXT_TWO}>MAC</h2>
+                </a>
+                <a href="https://github.com/B-P-Coders/covid-clicker-desktop/releases/download/v1.0.0/linux64.tar.gz">
+                    <h3 className={styles.downloadTEXT_THREE}><b>Linux</b></h3>
+                </a>
             </div>
         </div>
 
