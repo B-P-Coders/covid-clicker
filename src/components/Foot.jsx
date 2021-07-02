46136fc12b3993ef8ce643de6f3a2afad81e93a3
OreoPL-studio <81591050+OreoPL-studio@users.noreply.github.com>
2021-07-02 22:11:57 +0200

    Poprawa stopki
    jestem stupkarz

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -3,8 +3,7 @@ import * as styles from "../styles/Foot.module.css";
 export default function Foot() {
   return (
     <div className={styles.foot}>
-      © Copyright B&P Studio 2021
-      {/* Stopkę robi mistrz od stopek - Paweł */}
+      © Copyright B&P Studio {new Date().getFullYear()}
     </div>
   );
 }
